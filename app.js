9c6bbf753367f4757ab7b29ff458edab12d36adf
Darshik Hirapara <darshikhirapara@gmail.com>
2021-11-21 16:43:23 +0530

    added youtube services

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('dev'))
+
+const UserModel = require('./app/models/UserModel');
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === 'OPTIONS') {
+//       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//       return res.status(200).json({});
+//   }
+//   next();
+// });
 
 const dbConfig = require("./config/db.config.js");
 const mongoose = require("mongoose");
@@ -27,8 +44,17 @@ const connectWithDB = async () => {
 };
 connectWithDB();
 
-app.get("/", (req, res) => {
-  res.send(process.env.DB_URL);
+app.get("/", async (req, res) => {
+  var user = new UserModel({
+    email: "18ceuos102@gmail.com",
+  });
+  await user.save((err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(user);
+    }
+  });
 });
 
 app.listen(port, () => {
